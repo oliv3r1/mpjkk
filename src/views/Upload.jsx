8077a64d5a41f1a25be423c2838a188482d274dc
Oliv3r1 <oliver.suomela@metropolia.fi>
2023-04-06 10:20:46 +0300

    upload

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,15 +1,39 @@
 import {Box, Button} from '@mui/material';
 import PropTypes from 'prop-types';
 import useForm from '../hooks/FormHooks';
+import {useState} from 'react';
+import {useMedia} from '../hooks/ApiHooks';
+import {useNavigate} from 'react-router-dom';
 
 const Upload = (props) => {
+  const [file, setFile] = useState(null);
+  const {postMedia} = useMedia();
+  const navigate = useNavigate();
+
   const initValues = {
     title: '',
     description: '',
-    file: '',
   };
 
-  const doUpload = () => {};
+  const doUpload = async () => {
+    try {
+      const data = new FormData();
+      data.append('title', inputs.title);
+      data.append('description', inputs.description);
+      data.append('file', file);
+      const userToken = localStorage.getItem('userToken');
+      const uploadResult = await postMedia(data, userToken);
+      console.log('doUpload', uploadResult);
+      navigate('/home');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    event.persist();
+    setFile(event.target.files[0]);
+  };
 
   const {inputs, handleSubmit, handleInputChange} = useForm(
     doUpload,
@@ -18,7 +42,7 @@ const Upload = (props) => {
 
   return (
     <Box>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleInputChange}
           type="text"
@@ -29,14 +53,12 @@ const Upload = (props) => {
           onChange={handleInputChange}
           name="description"
           value={inputs.description}
-        >
-          {inputs.title}
-        </textarea>
+        ></textarea>
         <input
-          onChange={handleInputChange}
+          onChange={handleFileChange}
           type="file"
           name="file"
-          accept="image/* video/* audio/*"
+          accept="image/*,video/*,audio/*"
         ></input>
         <Button type="submit">Upload</Button>
       </form>
